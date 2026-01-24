4a2a1cf6cbaa8c631e8dab34cc3192e04f0a96e5
Daniele Lombardo <33962842+danielelombardo@users.noreply.github.com>
2026-01-24 12:56:55 +0100

    fix central peripheral battery level [no ci]

--- boards/shields/dongle_display/dongle_screen.c
+++ boards/shields/dongle_display/dongle_screen.c
@@ -9,6 +9,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/event_manager.h>
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/battery_state_changed.h>
+#include <zmk/split/central.h>
 #include <zmk/events/modifiers_state_changed.h>
 #include <zmk/keymap.h>
 #include <zmk/hid.h>
